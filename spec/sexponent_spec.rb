a8fa4d29b37f2e698812e9fe1e2050e3d018ee99
Bin7me <bin7me@gmail.com>
2013-03-11 18:12:33 +0100

    refactored spec for Sexponent

--- spec/sexponent_spec.rb
+++ spec/sexponent_spec.rb
@@ -6,25 +6,21 @@ require File.join(File.dirname(__FILE__), '..', '/lib/komponent.rb')
 require File.join(File.dirname(__FILE__), '..', '/komponents/sexponent.rb')
 
 describe Sexponent, "can_handle?(msgBag)" do
-  it "can handle a message bag with :content containing 'ex'" do
-    sexponent = Sexponent.new
+  sexponent = Sexponent.new
 
+  it "can handle a message bag with :content containing 'ex'" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small experiment")
       
     sexponent.can_handle?(msgBag).should eq(true)
   end
 
   it "can handle a message bag with :content containing 'Ex'" do
-    sexponent = Sexponent.new
-
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small Experiment")
 
     sexponent.can_handle?(msgBag).should eq(true) 
   end
 
   it "doesn't handle a message bag with :content containing neither containing 'ex' nor 'Ex'" do
-    sexponent = Sexponent.new
-
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small String")
 
     sexponent.can_handle?(msgBag).should eq(false) 
@@ -32,25 +28,21 @@ describe Sexponent, "can_handle?(msgBag)" do
 end
 
 describe Sexponent, "handle(msgBag)" do
-  it "replaces 'ex' with 'sex' in msgBag[:content] and returns it" do
-    sexponent = Sexponent.new
+  sexponent = Sexponent.new
 
+  it "replaces 'ex' with 'sex' in msgBag[:content] and returns it" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "ex")
 
     sexponent.handle(msgBag).should eq("sex")
   end
 
   it "replaces 'Ex' with 'Sex' in msgBag[:content] and returns it" do
-    sexponent = Sexponent.new
-
     msgBag = MsgBag.new(nil, nil, nil, nil, "Ex")
 
     sexponent.handle(msgBag).should eq("Sex")
   end
 
   it "returns msgBag[:content] if neither 'ex' nor 'Ex' are found" do
-    sexponent = Sexponent.new
-
     msgBag = MsgBag.new(nil, nil, nil, nil, "string")
 
     sexponent.handle(msgBag).should eq("string")
