fadad598718bcd84260d1f4823a07e97b028ed3e
Bin7me <bin7me@gmail.com>
2014-01-19 14:30:35 +0100

    ported bandname and sexponent specs to minitest

--- spec/bandName_spec.rb
+++ spec/bandName_spec.rb
@@ -5,6 +5,8 @@ require File.join(File.dirname(__FILE__), '..', '/lib/structs.rb')
 require File.join(File.dirname(__FILE__), '..', '/lib/komponent.rb')
 require File.join(File.dirname(__FILE__), '..', '/komponents/bandName.rb')
 
+require "minitest/autorun"
+
 describe BandName, "can_handle?(msgBag)" do
   bandName = BandName.new
   bandName.probability = 1.to_f
@@ -12,15 +14,15 @@ describe BandName, "can_handle?(msgBag)" do
   it "returns true for a message bag with :content consisting of three words" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "one two three")
 
-    bandName.can_handle?(msgBag).should eq(true)
+    bandName.can_handle?(msgBag).must_equal true
   end
 
   it "returns false for a message bag with :content having a wordcount != three" do
     msgBagTooShort = MsgBag.new(nil, nil, nil, nil, "one two")
     msgBagTooLong = MsgBag.new(nil, nil, nil, nil, "one two three four")
 
-    bandName.can_handle?(msgBagTooShort).should eq(false)
-    bandName.can_handle?(msgBagTooLong).should eq(false)
+    bandName.can_handle?(msgBagTooShort).must_equal false
+    bandName.can_handle?(msgBagTooLong).must_equal false
   end
 end
 
@@ -31,13 +33,13 @@ describe BandName, "handle(msgBag)" do
     bandName.probability = 1.to_f
     msgBag = MsgBag.new(nil, nil, nil, nil, "three word band")
 
-    bandName.handle(msgBag).should eq(">>three word band<< would be a nice name for a Rock Band!")
+    bandName.handle(msgBag).must_equal ">>three word band<< would be a nice name for a Rock Band!"
   end
 
   it "returns nil if probability is not met" do
     bandName.probability = -1.to_f
     msgBag = MsgBag.new(nil, nil, nil, nil, "three word band")
 
-    bandName.handle(msgBag).should eq(nil)
+    bandName.handle(msgBag).must_equal nil
   end
 end
--- spec/sexponent_spec.rb
+++ spec/sexponent_spec.rb
@@ -5,25 +5,27 @@ require File.join(File.dirname(__FILE__), '..', '/lib/structs.rb')
 require File.join(File.dirname(__FILE__), '..', '/lib/komponent.rb')
 require File.join(File.dirname(__FILE__), '..', '/komponents/sexponent.rb')
 
+require "minitest/autorun"
+
 describe Sexponent, "can_handle?(msgBag)" do
   sexponent = Sexponent.new
 
   it "returns true for a message bag with :content containing 'ex'" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small experiment")
       
-    sexponent.can_handle?(msgBag).should eq(true)
+    sexponent.can_handle?(msgBag).must_equal true
   end
 
   it "returns true for a message bag with :content containing 'Ex'" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small Experiment")
 
-    sexponent.can_handle?(msgBag).should eq(true) 
+    sexponent.can_handle?(msgBag).must_equal true
   end
 
   it "returns false for a message bag with :content containing neither 'ex' nor 'Ex'" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "A small String")
 
-    sexponent.can_handle?(msgBag).should eq(false) 
+    sexponent.can_handle?(msgBag).must_equal false
   end
 end
 
@@ -33,18 +35,18 @@ describe Sexponent, "handle(msgBag)" do
   it "replaces 'ex' with 'sex' in msgBag[:content] and returns it" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "ex")
 
-    sexponent.handle(msgBag).should eq("sex")
+    sexponent.handle(msgBag).must_equal "sex"
   end
 
   it "replaces 'Ex' with 'Sex' in msgBag[:content] and returns it" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "Ex")
 
-    sexponent.handle(msgBag).should eq("Sex")
+    sexponent.handle(msgBag).must_equal "Sex"
   end
 
   it "returns msgBag[:content] if neither 'ex' nor 'Ex' are found" do
     msgBag = MsgBag.new(nil, nil, nil, nil, "string")
 
-    sexponent.handle(msgBag).should eq("string")
+    sexponent.handle(msgBag).must_equal "string"
   end
 end
